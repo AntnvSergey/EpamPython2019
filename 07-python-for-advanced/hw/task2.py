09f014bff918b5962b1f690c8a6a9c930496a779
Sergey Antonov <serg2009141.antono@yandex.ru>
2020-01-09 00:06:03 +0300

    add hw python_for_advanced

--- 07-python-for-advanced/hw/task2.py
+++ 07-python-for-advanced/hw/task2.py
@@ -10,6 +10,51 @@ import time
 import uuid
 
 
+def timer_property(t):
+
+    time_to_reset = t
+
+    class CacheProperty(object):
+        def __init__(self, fget=None, fset=None, fdel=None):
+            self.fget = fget
+            self.fset = fset
+            self.fdel = fdel
+            self.current_time = 0
+            self.cache = 0
+
+        def __get__(self, obj, objtype=None):
+            if obj is None:
+                return self
+            if self.fget is None:
+                raise AttributeError("unreadable attribute")
+            if self.current_time == 0 or time.time() - self.current_time > time_to_reset:
+                self.cache = self.fget(obj)
+                self.current_time = time.time()
+            return self.cache
+
+        def __set__(self, obj, value):
+            if self.fset is None:
+                raise AttributeError("Can't set attribute")
+            self.current_time = time.time()
+            self.cache = value
+
+        def __delete__(self, obj):
+            if self.fdel is None:
+                raise AttributeError("Can't delete attribute")
+            self.fdel(obj)
+
+        def getter(self, fget):
+            return type(self)(fget, self.fset, self.fdel)
+
+        def setter(self, fset):
+            return type(self)(self.fget, fset, self.fdel)
+
+        def deleter(self, fdel):
+            return type(self)(self.fget, self.fset, fdel)
+
+    return CacheProperty
+
+
 class Message:
 
     @timer_property(t=10)
@@ -25,12 +70,12 @@ class Message:
         """
         Return random string
         """
-        return uuid.uuid4().get_hex()
+        return uuid.uuid4().hex
 
 
 if __name__ == '__main__':
     m = Message()
-    initial = m.read
-    assert initial is m.read
-    time.sleep(10)
-    assert initial is not m.read
+    initial = m.msg
+    assert initial is m.msg
+    time.sleep(2)
+    assert initial is not m.msg
